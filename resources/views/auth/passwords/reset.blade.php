95c8346f80498e6d64146a237573bd012e2029c2
Eduardo <edward.axl3@gmail.com>
2019-12-08 15:33:13 -0500

    agrenado funcionalidad para recuperar contraseñas 5

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -17,7 +17,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus disabled>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus readonly>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
