93fe89030d0fb4d76ea486b40526a9767591170c
Eduardo <echuquillanquiy@gmail.com>
2020-03-11 10:04:54 -0500

    actualizando tamaño de tabal en formato 006 - 4

--- resources/views/tracings/impresion.blade.php
+++ resources/views/tracings/impresion.blade.php
@@ -12,7 +12,7 @@
 
 <p style="margin-left:-5px; font-size:0.7rem; font-family:Arial, Helvetica, sans-serif;"><strong>I. DATOS DE FILIACIÓN</strong></p>
 
-<table width="100%" style="margin-top:-8px; margin-left:-5px; margin-right:-1px; font-family:Arial, Helvetica, sans-serif; font-size:0.6rem; border-collapse:collapse">
+<table width="95%" style="margin-top:-8px; margin-left:-5px; margin-right:-1px; font-family:Arial, Helvetica, sans-serif; font-size:0.6rem; border-collapse:collapse">
     <tr>
         <td style="margin-right:5px !important; width:90px">NOMBRE DEL PACIENTE:</strong></td>
         <td style="text-align: center !important; border: 1px solid !important; width:200px;height:20px !important">{{$tracing->patient->name }}</td>
