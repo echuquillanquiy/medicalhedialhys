353fc97383354bb4b361e6d655e94ed1be3886f8
echuquillanquiy <edward.axl3@gmail.com>
2022-12-02 17:49:47 -0500

    CAMBIANDO TITULO DE HEDIAL HEMO SALUD

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
-
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
@@ -16,6 +16,7 @@
   <link href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
   <!-- Argon CSS -->
   <link href="{{ asset('css/argon.css?v=1.0.0') }}" rel="stylesheet">
+
 </head>
 
 <body class="bg-default">
